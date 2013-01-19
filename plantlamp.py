4c9b415a38612afe693b3615ab31da532604bed5
Gabriel Nadler <nadlergabriel@gmail.com>
2013-01-19 18:12:48 +0100

    Test 8.2 - hopefully working

--- plantlamp.py
+++ plantlamp.py
@@ -7,6 +7,7 @@ from HTMLParser import HTMLParser
 import urllib
 import os
 import re
+import RPi.GPIO
 # import RPi.GPIO
 Config = ConfigParser.ConfigParser()
 LAMP_ONE = 7
@@ -39,7 +40,6 @@ def __init__():
     RPI_ON = module_exists("RPi")
     LOGGER = Logger(LOGFILE, "Main")
     if RPI_ON:
-        import RPi.GPIO
         LAMP_ONE = Lamp(LAMP_ONE, RPI_ON)
     else:
         print "NO RPI Present"
